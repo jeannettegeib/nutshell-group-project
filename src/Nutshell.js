35f1c406a6996d050481632bf414256a7059b510
TabathaSlater <106984127+TabathaSlater@users.noreply.github.com>
2022-08-15 13:56:23 -0400

    register, login, views

---
+++ src/Nutshell.js
@@ -0,0 +1,22 @@
+import { Route, Routes } from "react-router-dom"
+import { Login } from "./components/auth/Login"
+import { Register } from "./components/auth/Register"
+import { ApplicationViews } from "./components/views/ApplicationViews"
+import { Authorized } from "./components/views/Authorized"
+
+
+
+export const Nutshell = () => {
+	return (<Routes>
+		<Route path="/login" element={<Login />} />
+		<Route path="/register" element={<Register />} />
+    <Route path="*" element={
+			<Authorized>
+				<>
+					<ApplicationViews />
+				</>
+			</Authorized>
+
+		} />
+	</Routes>)
+}
---
+++ src/components/auth/Login.js
@@ -0,0 +1,69 @@
+import React, { useState } from "react"
+import { Link } from "react-router-dom";
+import { useNavigate } from "react-router-dom"
+
+
+export const Login = () => {
+    const [email, set] = useState("")
+    const [username, setUsername] =useState("")
+    const navigate = useNavigate()
+
+    const handleLogin = (e) => {
+        e.preventDefault()
+
+        return fetch(`http://localhost:8088/users?email=${email}`)
+            .then(res => res.json())
+            .then(foundUsers => {
+                if (foundUsers.length === 1) {
+                    const user = foundUsers[0]
+                    localStorage.setItem("nutshell_user", JSON.stringify({
+                        id: user.id,
+                        username: user.username,
+                        email: user.email
+                    }))
+
+                    navigate("/")
+                }
+                else {
+                    window.alert("Invalid login")
+                }
+            })
+    }
+
+    return (
+        <main className="container--login">
+            <section>
+                <form className="form--login" onSubmit={handleLogin}>
+                    <h1>Nutshell</h1>
+                    <h2>Please sign in</h2>
+                    <fieldset>
+                        <label htmlFor="inputUsername"> Username </label>
+                        <input type="username"
+                            value={username}
+                            onChange={evt => setUsername(evt.target.value)}
+                            className="form-control"
+                            placeholder="username"
+                            required autoFocus />
+                    </fieldset>
+                    <fieldset>
+                        <label htmlFor="inputEmail"> Email address </label>
+                        <input type="email"
+                            value={email}
+                            onChange={evt => set(evt.target.value)}
+                            className="form-control"
+                            placeholder="Email address"
+                            required autoFocus />
+                    </fieldset>
+                    <fieldset>
+                        <button type="submit">
+                            Sign in
+                        </button>
+                    </fieldset>
+                </form>
+            </section>
+            <section className="link--register">
+                <Link to="/register">Not a member yet?</Link>
+            </section>
+        </main>
+    )
+}
